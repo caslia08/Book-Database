7eaa7fd965b0b69211e843e92b7dce200697709e
caslia08 <caslia08@gmail.com>
2018-11-26 12:31:57 +0200

    Init commit

---
+++ BooksDatabase/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace BooksDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_title = new System.Windows.Forms.TextBox();
+            this.txt_year = new System.Windows.Forms.TextBox();
+            this.txt_ISBN = new System.Windows.Forms.TextBox();
+            this.txt_PubID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_title
+            // 
+            this.txt_title.Location = new System.Drawing.Point(148, 25);
+            this.txt_title.Name = "txt_title";
+            this.txt_title.Size = new System.Drawing.Size(178, 20);
+            this.txt_title.TabIndex = 0;
+            // 
+            // txt_year
+            // 
+            this.txt_year.Location = new System.Drawing.Point(148, 67);
+            this.txt_year.Name = "txt_year";
+            this.txt_year.Size = new System.Drawing.Size(178, 20);
+            this.txt_year.TabIndex = 1;
+            // 
+            // txt_ISBN
+            // 
+            this.txt_ISBN.Location = new System.Drawing.Point(148, 104);
+            this.txt_ISBN.Name = "txt_ISBN";
+            this.txt_ISBN.Size = new System.Drawing.Size(178, 20);
+            this.txt_ISBN.TabIndex = 2;
+            // 
+            // txt_PubID
+            // 
+            this.txt_PubID.Location = new System.Drawing.Point(148, 140);
+            this.txt_PubID.Name = "txt_PubID";
+            this.txt_PubID.Size = new System.Drawing.Size(178, 20);
+            this.txt_PubID.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Year Published";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ISBN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Publisher ID";
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(8, 180);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(75, 23);
+            this.btnFirst.TabIndex = 8;
+            this.btnFirst.Text = "First";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(89, 180);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 9;
+            this.btnPrev.Text = "Previous";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(170, 180);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 10;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(251, 180);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(75, 23);
+            this.btnLast.TabIndex = 11;
+            this.btnLast.Text = "Last";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 228);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_PubID);
+            this.Controls.Add(this.txt_ISBN);
+            this.Controls.Add(this.txt_year);
+            this.Controls.Add(this.txt_title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_title;
+        private System.Windows.Forms.TextBox txt_year;
+        private System.Windows.Forms.TextBox txt_ISBN;
+        private System.Windows.Forms.TextBox txt_PubID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnLast;
+    }
+}
+
---
+++ BooksDatabase/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BooksDatabase
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection conn;
+        OleDbCommand Title_command;
+        OleDbDataAdapter Titles_Adapter;
+        DataTable Table_Titles;
+        CurrencyManager TitlesManager;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var connstring = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Alia\Desktop\Books.accdb;
+Persist Security Info = False; ";
+            conn = new OleDbConnection(connstring);
+            conn.Open();
+            Title_command = new OleDbCommand("Select * from Titles", conn);
+            Titles_Adapter = new OleDbDataAdapter();
+            Titles_Adapter.SelectCommand = Title_command;
+            Table_Titles = new DataTable();
+            Titles_Adapter.Fill(Table_Titles);
+
+            //Data Binding control
+
+            txt_title.DataBindings.Add("Text", Table_Titles, "Title");
+            txt_year.DataBindings.Add("Text", Table_Titles, "Year_Published");
+            txt_ISBN.DataBindings.Add("Text", Table_Titles, "ISBN");
+            txt_PubID.DataBindings.Add("Text", Table_Titles, "PubId");
+
+            //establish currency manager
+
+            TitlesManager = (CurrencyManager)BindingContext[Table_Titles];
+            // can't implicitly therefore will cast it 
+            // bound data passed to currency manager- creates an array with the data
+
+            conn.Close();
+            conn.Dispose();
+            Titles_Adapter.Dispose();
+            Table_Titles.Dispose();
+
+
+
+            
+            
+
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            TitlesManager.Position = 0;
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            TitlesManager.Position--; ;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            TitlesManager.Position++;
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            TitlesManager.Position = TitlesManager.Count + 1;
+            //Array starts at ZERO
+        }
+    }
+}
